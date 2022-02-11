b1a5b2a3977389766efa5a107ca008a1f1a32017
iAdrian14 <58402454+iAdrian14@users.noreply.github.com>
2022-02-10 18:38:11 -0600

    Adrian parte finalizado

---
+++ PrimerPeriodo/src/main/java/Portafolio1/Medicina.java
@@ -0,0 +1,28 @@
+
+package Portafolio1;
+
+/**
+ *
+ * @author Leonidas Adrian Mendoza Flores
+ */
+public class Medicina {
+    String descripcion;
+    char idMedicina;
+    
+    public String setDescripcion()
+    {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion)
+    {
+        this.descripcion = descripcion;
+    }
+    public char getIdMedicina()
+    {
+        return idMedicina;
+    }
+    public void setIdMedicina(char id)
+    {
+        this.idMedicina = id;
+    }
+}
---
+++ PrimerPeriodo/src/main/java/Portafolio1/SolicitudCita.java
@@ -0,0 +1,67 @@
+package Portafolio1;
+
+import java.util.Date;
+/**
+ *
+ * @author Leonidas Adrian Mendoza Flores
+ */
+public class SolicitudCita {
+    char idSolicitud;
+    char estado;
+    Date fecha;
+    Date hora;
+    String nombreMedico;
+    char tipoConsulta;
+    
+    public char getIdSolicitud() 
+    {
+         return idSolicitud;
+    }
+  
+    public void setIdSolicitud(char id) 
+    {
+        this.idSolicitud = id;
+    }
+    public char getEstado() 
+    {
+        return estado;
+    }
+    public void setEstado(char estado) 
+    {
+        this.estado = estado;
+    }
+    public Date getFecha() 
+    {
+        return fecha;
+    } 
+    public void setFecha(Date fecha) 
+    {
+        this.fecha = fecha;
+    } 
+    public Date getHora() 
+    {
+        return hora;
+    } 
+    public void setHora(Date hora) 
+    {
+        this.hora = hora;
+    } 
+    public String getMedicoNombre() 
+    {
+        return nombreMedico;
+    }
+    public void setNombreMedico(String medico) 
+    {
+        this.nombreMedico = medico;
+    }
+  
+    public char getTipoConsulta() 
+    {
+        return tipoConsulta;
+    }
+  
+     public void setTipoConsulta(char tipo) 
+    {
+        this.tipoConsulta = tipo;
+    }
+}
