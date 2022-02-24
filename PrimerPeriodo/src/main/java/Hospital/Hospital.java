c4f11f3a69631ad60862b30af35c5ff03d2239df
iAdrian14 <58402454+iAdrian14@users.noreply.github.com>
2022-02-23 20:42:28 -0600

    24

--- PrimerPeriodo/src/main/java/Hospital/Hospital.java
+++ PrimerPeriodo/src/main/java/Hospital/Hospital.java
@@ -10,11 +10,23 @@ public class Hospital {
         Scanner entrada = new Scanner(System.in);
         boolean salir = true;
         while(salir){
+            System.out.println("Hospotal El Salvador\nIngrese una opicion:"
+                    + "\n1 - Citas"
+                    + "\n2 - Medicos"
+                    + "\n3 - Pacientes"
+                    + "\n4 - Medicinas"
+                    + "\n5 - Salir");
             int opcion = entrada.nextInt();
             switch(opcion){
-                case 1:{
+                case 1:
                     System.out.println("Hola");
-                }
+                    break;
+                case 5:
+                    salir = false;
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion valida");
+                    break;
             }
         }
     }
